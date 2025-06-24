b7a9c89f050a0f3a5a65db01851a5bb4d6ff1184
Dan Shaw <dcshaw.en@gmail.com>
2025-06-24 18:13:44 -0300

    Adds verbosity to rundev.sh

--- scripts/rundev.sh
+++ scripts/rundev.sh
@@ -2,5 +2,7 @@
 
 # This script is currently only compatible with flask and vue that are running within WSL2 terminals. If installed on a full linux distribution or using powershell, this script will not work
 
+echo "Starting virtual environment and flask server.. ..."
 wt.exe bash -ic ". venv/bin/activate && flask --app recipefirst --debug run --host=0.0.0.0"
-wt.exe bash -ic "cd www/ && npm run dev"
+echo "Start vue server.. ..."
+wt.exe bash -ic "cd www/ && npm run dev -- --host"
