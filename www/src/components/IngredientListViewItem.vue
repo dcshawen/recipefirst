6323c9fbe5323dee0d06e21d45a6e1f9cb21fb1e
Dan Shaw <dcshaw.en@gmail.com>
2025-08-13 18:44:39 -0300

    REF-26 Implements IngredientListView

---
+++ www/src/components/IngredientListViewItem.vue
@@ -0,0 +1,18 @@
+<template>
+	<li class="ingredient-list-item">
+		<span class="ingredient-name">{{ ingredient.name }}</span>
+		<span class="ingredient-amount">{{ ingredient.amount }}</span>
+		<span class="ingredient-unit">{{ ingredient.unit }}</span>
+	</li>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+	ingredient: {
+		type: Object,
+		required: true
+	}
+})
+</script>
