98fe9af901e1b36d9d3778321b34ca4bd64eae4d
Dan Shaw <dcshaw.en@gmail.com>
2025-08-13 19:08:02 -0300

    REF-26 Implements Ingredient List View with parameters

--- www/src/components/IngredientListViewItem.vue
+++ www/src/components/IngredientListViewItem.vue
@@ -1,14 +1,14 @@
 <template>
-	<li class="ingredient-list-item">
-		<span class="ingredient-name">{{ ingredient.name }}</span>
-		<span class="ingredient-amount">{{ ingredient.amount }}</span>
-		<span class="ingredient-unit">{{ ingredient.unit }}</span>
+	<li class="ingredient-list-item flex items-center justify-between p-2 border-b">
+		<span>
+			<span class="ingredient-name font-bold pr-2">{{ ingredient.name }}</span>
+			<span class="ingredient-notes text-gray-400 text-[0.5em]">{{ ingredient.description }}</span>
+		</span>
+		<span class="ingredient-description text-gray-500">{{ ingredient.created_at }}</span>
 	</li>
 </template>
 
 <script setup>
-import { defineProps } from 'vue'
-
 const props = defineProps({
 	ingredient: {
 		type: Object,
