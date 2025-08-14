fd8086a970a79f36feafd6b9375bc3303fd2a5f4
Dan Shaw <dcshaw.en@gmail.com>
2025-08-14 19:48:46 -0300

    REF-27 Implements IngredientDetails view

---
+++ www/src/pages/IngredientDetails.vue
@@ -0,0 +1,18 @@
+<template>
+  <div v-if="ingredient" class="p-4 m-6 bg-gray-100 rounded">
+    <div class="font-bold mb-2">Ingredient Details:</div>
+    <div><span class="font-semibold">Name:</span> {{ ingredient.ingredient_name }}</div>
+    <div><span class="font-semibold">Description:</span> {{ ingredient.ingredient_description }}</div>
+    <div><span class="font-semibold">Notes:</span> {{ ingredient.ingredient_notes }}</div>
+    <div><span class="font-semibold">Created On:</span> {{ ingredient.created_at }}</div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  ingredient: {
+    type: Object,
+    required: true
+  }
+});
+</script>
