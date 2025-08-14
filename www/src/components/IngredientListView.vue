50ad97e496ba91146c4d0a69158f0f1dc55aa0c8
Dan Shaw <dcshaw.en@gmail.com>
2025-08-14 18:56:35 -0300

    REF-53 Implements GenericListView to replace IngredientListView

--- www/src/components/IngredientListView.vue
+++ www/src/components/IngredientListView.vue
@@ -1,19 +1,34 @@
 <template>
 	<div class="ingredient-list-view">
-		<IngredientListViewItem
-			v-for="(ingredient, index) in ingredients"
+		<div class="ingredient-list-header flex">
+			<span
+				v-for="(column, idx) in ingredientData.columns"
+				:key="column.key || idx"
+				class="ingredient-list-column"
+			>
+				{{ column.name }}
+			</span>
+		</div>
+		<div
+			v-for="(ingredient, index) in ingredientData.ingredients"
 			:key="ingredient.id || index"
-			:ingredient="ingredient"
-		/>
+			class="ingredient-list-row flex"
+		>
+			<span
+				v-for="(column, idx) in ingredientData.columns"
+				:key="column.key || idx"
+				class="ingredient-list-cell"
+			>
+				{{ ingredient[column.field] }}
+			</span>
+		</div>
 	</div>
 </template>
 
 <script setup>
-import IngredientListViewItem from './IngredientListViewItem.vue'
-
 defineProps({
-	ingredients: {
-		type: Array,
+	ingredientData: {
+		type: Object,
 		required: true
 	}
 })
@@ -24,5 +39,26 @@ defineProps({
 	display: flex;
 	flex-direction: column;
 	gap: 8px;
+}
+.ingredient-list-header,
+.ingredient-list-row {
+	display: flex;
+}
+.ingredient-list-column,
+.ingredient-list-cell {
+	flex: 1 1 0;
+	padding: 0.5em;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	white-space: nowrap;
+}
+.ingredient-list-header {
+	font-weight: bold;
+	background: #f9fafb;
+	border-bottom: 1px solid #e5e7eb;
+}
+.ingredient-list-row {
+	border-bottom: 1px solid #e5e7eb;
+	align-items: center;
 }
 </style>
