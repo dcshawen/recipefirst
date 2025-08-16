c2072f9174c39791e0b8806970f0862c065d181b
Dan Shaw <dcshaw.en@gmail.com>
2025-08-16 19:48:25 -0300

    REF-29 Integrates backend API and eliminates reliance on json test data

--- www/src/views/GenericListView.vue
+++ www/src/views/GenericListView.vue
@@ -1,25 +1,28 @@
 <template>
 	<div class="flex flex-col w-full">
-		<div class="flex bg-gray-100 font-bold">
-			<div
-				v-for="(column, idx) in itemData.columns"
-				:key="idx"
-				class="flex-1 px-2 py-2"
-			>
-				{{ column.label }}
+		<h1 class="text-2xl px-2">{{ formattedType }}</h1>
+		<div class="flex flex-col w-full my-4">
+			<div class="flex bg-gray-100 font-bold">
+				<div
+					v-for="(column, idx) in itemData.columns"
+					:key="idx"
+					class="flex-1 px-2 py-2"
+				>
+					{{ column.label }}
+				</div>
 			</div>
-		</div>
-		<div
-			v-for="(item, index) in itemData.items"
-			:key="item.id || index"
-			class="flex bg-white"
-		>
 			<div
-				v-for="(column, idx) in itemData.columns"
-				:key="idx"
-				class="flex-1 px-2 py-2"
+				v-for="(item, index) in itemData.items"
+				:key="item.id || index"
+				class="flex bg-white"
 			>
-				{{ item[column.field] }}
+				<div
+					v-for="(column, idx) in itemData.columns"
+					:key="idx"
+					class="flex-1 px-2 py-2"
+				>
+					{{ item[column.field] }}
+				</div>
 			</div>
 		</div>
 	</div>
@@ -33,6 +36,16 @@ export default {
 			type: Object,
 			required: true,
 		},
+		type: {
+			type: String,
+			default: "Items"
+		}
 	},
+	computed: {
+		formattedType() {
+			if (!this.type) return "Items";
+			return this.type.charAt(0).toUpperCase() + this.type.slice(1);
+		}
+	}
 };
 </script>
