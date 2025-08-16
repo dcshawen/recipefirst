5ece37724cea4c99fc8e77c1e5a0ceb309364343
Dan Shaw <dcshaw.en@gmail.com>
2025-08-16 09:33:24 -0300

    REF-22 Implements REF-25 REF-34 REF-35

---
+++ www/src/templates/MainMenu.vue
@@ -0,0 +1,12 @@
+<template>
+	<nav class="flex flex-col space-y-4 p-2 bg-gray-100 w-40 min-h-screen">
+		<button @click="$emit('showHome')">Home</button>
+		<button @click="$emit('showRandomIngredient')">Ingredient Details</button>
+		<button @click="$emit('showRandomRecipe')">Recipe Details</button>
+		<button @click="$emit('showRandomMeal')">Meal Details</button>
+	</nav>
+</template>
+
+<script setup>
+// Remove all logic and refs, only emit events
+</script>
