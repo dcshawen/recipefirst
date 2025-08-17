366e112c317bce35e1c5ff15db5e846abd7d759d
Dan Shaw <dcshaw.en@gmail.com>
2025-08-17 16:25:23 -0300

    Adjusts version information in footer

--- www/src/templates/Footer.vue
+++ www/src/templates/Footer.vue
@@ -1,6 +1,6 @@
 <template>
-	<footer class="bg-gray-800 text-white p-4">
+	<footer class="flex justify-between bg-gray-800 text-white p-4">
 		<p class="text-sm">© 2025 Dan Shaw</p>
-		<p class="text-xs">data: 0.0.1 www: 0.0.1</p>
+		<p class="text-xs">app 0.0.2 | data 0.0.1 | www 0.0.1</p>
 	</footer>
 </template>
