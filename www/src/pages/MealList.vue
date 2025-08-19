d38d95849ac37875aa95e4bc95f7e3ae17177968
Dan Shaw <dcshaw.en@gmail.com>
2025-08-19 19:44:23 -0300

    REF-83 REF-86 REF-87 REF-88 REF-89 Implements GenericList.vue to replace GenericListView.vue

---
+++ www/src/pages/MealList.vue
@@ -0,0 +1,28 @@
+<template>
+	<GenericList :config="mealConfig" />
+</template>
+
+<script setup>
+import GenericList from '../components/GenericList.vue'
+
+const mealConfig = {
+	endpoint: '/meals',
+	dataKey: 'meals',
+	idField: 'meal_id',
+	routePrefix: 'meals',
+	columns: [
+		{
+			key: 'name',
+			label: 'Meal Name',
+			field: 'meal_name',
+			type: 'link'
+		},
+		{
+			key: 'updated',
+			label: 'Last Updated',
+			field: 'updated_at',
+			type: 'date'
+		}
+	]
+}
+</script>
